8bbdb30b4891f8952b686f79417037f608e8d7fd
BhargavMallikB <bhargavmallikb@gmail.com>
2025-03-28 12:34:39 +0530

    chore: create and add Product

---
+++ src/app/types/Product.ts
@@ -0,0 +1,11 @@
+export interface Product{
+    id: number,
+    title: string,
+    category: string,
+    price: number,
+    stock: number,
+    available: boolean,
+    imageUrl: string,
+    rating: number,
+    description: string
+}
